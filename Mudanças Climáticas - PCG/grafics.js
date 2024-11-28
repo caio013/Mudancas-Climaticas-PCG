7525d849e5e363e9a2c75fcc044e2492d33353d9
Caio Vinicius <caiovinicius96@yahoo.com>
2024-11-28 01:13:17 -0300

    Adicionando gráfico e estilização do site

--- Mudanças Climáticas - PCG/grafics.js
+++ Mudanças Climáticas - PCG/grafics.js
@@ -1,20 +1,61 @@
-var ctx = document.getElementsByClassName('myChart')
+var ctx = document.getElementById('myChart').getContext('2d')
 
-var chartGraph = new Chart(ctx, {
+var chart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: [
-      'Jan',
-      'Fev',
-      'Mar',
-      'Abri',
-      'Mai',
-      'Jun',
-      'Jul',
-      'Ago',
-      'Out',
-      'Nov',
-      'Dez'
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezzembro'
+    ],
+    datasets: [
+      {
+        label: '# of Votes',
+        backgroundColor: ['blue', 'yellow', 'pink', 'red', 'green', 'gray'],
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 10,
+        borderColor: 'gray'
+      }
     ]
-  }
+  },
+  options: {}
+})
+
+var chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezzembro'
+    ],
+    datasets: [
+      {
+        label: '# of Votes',
+        backgroundColor: ['blue', 'yellow', 'pink', 'red', 'green', 'gray'],
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 10,
+        borderColor: 'gray'
+      }
+    ]
+  },
+  options: {}
 })
